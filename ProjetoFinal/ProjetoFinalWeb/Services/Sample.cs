fa1e503e142905db5ab7ea6a02111a58eceb57b4
Jonatan Machado <jonatan.mac@gmail.com>
2016-08-08 09:56:14 -0300

    estrutura inicial do projeto

---
+++ ProjetoFinal/ProjetoFinalWeb/Services/Sample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace ProjetoFinalWeb.Services
+{
+    /// <summary>
+    /// Essa é uma classe de serviço e deve ser usada como exemplo.
+    /// </summary>
+    public class Sample
+    {
+        /// <summary>
+        /// Esse é um método de exemplo escrito de forma simplificada.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public string GetFilmsByName(string name)
+        {   
+            HttpClient http = new HttpClient();
+            var result = http.GetStringAsync("http://url_da_api?name=" + name).Result;
+
+            return result;
+        }
+    }
+}
---
+++ ProjetoFinal/ProjetoFinalWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoFinalWeb.Startup))]
+namespace ProjetoFinalWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
