6302ab0f480733b73755b7f2f76d67663dc6c520
Jonatan Machado <jonatan.mac@gmail.com>
2016-08-13 11:29:49 -0300

    Refatoração do Filme controller

--- ProjetoFinal/ProjetoFinalWeb/Controllers/FilmesController.cs
+++ ProjetoFinal/ProjetoFinalWeb/Controllers/FilmesController.cs
@@ -8,51 +8,24 @@ using System.Web.Mvc;
 using ProjetoFinalWeb.Models;
 using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
+using ProjetoFinalWeb.Services;
 
 namespace ProjetoFinalWeb.Controllers
 {
-    /// <summary>
-    /// http://img.omdbapi.com/?i=tt2294629&apikey=35652245
-    /// Passar a utilizar essa chave a partir de amanhã.
-    /// Eu fiz o cadastro no site pra ter uma chave
-    /// ------  API que retorna 1 filme apenas
-    /// ------  string url = "http://www.omdbapi.com/?t=" + nome + "&y=&plot=short&r=json";
-    /// </summary>
+   
     public class FilmesController : Controller
     {
-        private List<FilmesModel> itens;
-        private readonly string BASE_URI = "http://www.omdbapi.com/?s={0}&type=movie";
-
+       
         public ActionResult Index()
-        {
-            itens = new List<FilmesModel>();
-            return View(itens);
+        {   
+            return View(Enumerable.Empty<FilmesModel>());
         }
 
         [HttpPost]
         public async Task<ActionResult> Index(string nome)
         {
-            string url = string.Format(BASE_URI, nome);
-
-            try
-            {
-                System.Net.Http.HttpClient http = new System.Net.Http.HttpClient();
-                var json = await http.GetStringAsync(url);
-                var result = JObject.Parse(json);
-                itens = JsonConvert.DeserializeObject<List<FilmesModel>>(result.GetValue("Search").ToString());
-
-                if (itens.Any(lambda => String.IsNullOrEmpty(lambda.Poster)))
-                {
-                    ViewBag.paraoErro = 1;
-                    return View();
-                }
-            }
-            catch (WebException)
-            {
-                ViewBag.Erro = "Conexão Indisponivel";
-            }
-
-            return View(itens);
+            OMDService service = new OMDService();
+            return View(await service.ObterFilmesPorNome(nome));
         }
     }
 }
---
+++ ProjetoFinal/ProjetoFinalWeb/Services/OMDService.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ProjetoFinalWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoFinalWeb.Services
+{
+    /// <summary>
+    /// http://img.omdbapi.com/?i=tt2294629&apikey=35652245
+    /// Passar a utilizar essa chave a partir de amanhã.
+    /// Eu fiz o cadastro no site pra ter uma chave
+    /// ------  API que retorna 1 filme apenas
+    /// ------  string url = "http://www.omdbapi.com/?t=" + nome + "&y=&plot=short&r=json";
+    /// </summary>
+    public class OMDService
+    {
+        private List<FilmesModel> itens;
+        private readonly string BASE_URI = "http://www.omdbapi.com/?s={0}&type=movie";
+
+        public async Task<List<FilmesModel>> ObterFilmesPorNome(string nome)
+        {
+            string url = string.Format(BASE_URI, nome);
+
+            System.Net.Http.HttpClient http = new System.Net.Http.HttpClient();
+            var json = await http.GetStringAsync(url);
+            var result = JObject.Parse(json);
+            itens = JsonConvert.DeserializeObject<List<FilmesModel>>(result.GetValue("Search").ToString());
+            
+            return itens;            
+        }
+    }
+}
