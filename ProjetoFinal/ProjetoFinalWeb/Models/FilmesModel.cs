16ae9378ee3a6d9d3f1e8b21b1c6fcdd4a6278c1
Sandro Oliveira <sock.adm@gmail.com>
2016-09-01 00:01:43 -0300

    Inclusao de mais informações no detalhe dos filmes
    Ajuste no Filme model, e inclusão destas informações para serem exibidas
    na view de detalhes do filme

--- ProjetoFinal/ProjetoFinalWeb/Models/FilmesModel.cs
+++ ProjetoFinal/ProjetoFinalWeb/Models/FilmesModel.cs
@@ -7,32 +7,32 @@ namespace ProjetoFinalWeb.Models
 {
     public class FilmesModel
     {
-        public string Title { get; set; }
-        public string Year { get; set; }
-        public string imdbID { get; set; }
-        public string Type { get; set; }
-        public string Poster { get; set; }
+        //public string Title { get; set; }
+        //public string Year { get; set; }
+        //public string imdbID { get; set; }
+        //public string Type { get; set; }
+        //public string Poster { get; set; }
         public string Response { get; set; }
 
         // MODEL PARA API QUE RETORNA 1 FILME
-        //public string Title { get; set; }
-        //public string Year { get; set; }
-        //public string Rated { get; set; }
-        //public string Released { get; set; }
-        //public string Runtime { get; set; }
-        //public string Genre { get; set; }
-        //public string Director { get; set; }
-        //public string Writer { get; set; }
-        //public string Actors { get; set; }
-        public string Plot { get; set; }
-        //public string Language { get; set; }
-        //public string Country { get; set; }
-        //public string Awards { get; set; }
-        //public string Poster { get; set; }
-        //public string Metascore { get; set; }
-        //public string imdbRating { get; set; }
-        //public string imdbVotes { get; set; }
-        //public string imdbID { get; set; }
-        //public string Type { get; set; }
+        public string Title { get; set; } //ok
+        public string Year { get; set; }   //ok
+        public string Rated { get; set; }
+        public string Released { get; set; } //ok
+        public string Runtime { get; set; } //ok
+        public string Genre { get; set; }  //ok
+        public string Director { get; set; } //ok
+        public string Writer { get; set; } //ok
+        public string Actors { get; set; } //ok
+        public string Plot { get; set; } //ok
+        public string Language { get; set; }
+        public string Country { get; set; }
+        public string Awards { get; set; }
+        public string Poster { get; set; }
+        public string Metascore { get; set; }
+        public string imdbRating { get; set; }
+        public string imdbVotes { get; set; }
+        public string imdbID { get; set; } //ok
+        public string Type { get; set; }
     }
 }
